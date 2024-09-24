82548a1015ef4852738747052307e34499bd8f93
N1ph1l1m <zerorad@inbox.ru>
2024-09-24 16:28:01 +0300

    Version 0.19.6 update layout chats

--- src/Shared/Message/message.jsx
+++ src/Shared/Message/message.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../Message/message.scss"
 
-export default function Message({text,sent}){
+export default function Message({text,sent,time}){
     return(
         <div className={`message ${sent ? 'sent':'received'}`}>
             <div className="message-bubble">
-                {text}
+                <div className="bubble-text">{text}</div>
+                <div className="bubble-time-wrap"><span className="bubble-time-text">{time}</span></div>
              </div>
         </div>
     )
