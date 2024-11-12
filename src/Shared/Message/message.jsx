07608dbaf16d6fe5427cceb271950ac5c592fc1a
N1ph1l1m <zerorad@inbox.ru>
2024-11-12 14:36:29 +0200

    Version 0.37 refactor  message.jsx part 2

--- src/Shared/Message/message.jsx
+++ src/Shared/Message/message.jsx
@@ -3,6 +3,83 @@ import styles from "../../App/Styles/message.module.css";
 import Picker from "emoji-picker-react";
 import { FaArrowCircleDown } from "react-icons/fa";
 import Icon from "../icon/icon";
+
+const HeaderName = ({username}) => (
+  <div className={styles.bubbleNameWrap}>
+    <span className={styles.bubbleNameText}>{username}</span>
+  </div>
+);
+const ItemTime = ({time}) => (
+  <div className={styles.bubbleTimeWrap}>
+    <span className={styles.bubbleTimeText}>{time}</span>
+  </div>
+);
+const ShowPhoto = ({ photos, modalPhoto, photoData }) => {
+  if (!Array.isArray(photos) || photos.length === 0) return null;
+
+  let wrapClass = styles.wrapPhoto;
+  let photoClass = styles.photoMessage;
+
+  if (photos.length === 2) {
+    wrapClass = styles.wrapPhotoTwo;
+    photoClass = styles.photoMessageTwoPhoto;
+  } else if (photos.length > 2 && photos.length % 2 === 0) {
+    wrapClass = styles.wrapPhotoEven;
+    photoClass = styles.photoMessageEven;
+  } else if (photos.length > 2) {
+    wrapClass = styles.wrapPhotoOdd;
+    photoClass = styles.photoMessageOdd;
+  }
+
+  return (
+    <div className={wrapClass}>
+      {photos.map((photo, index) => (
+        // eslint-disable-next-line jsx-a11y/img-redundant-alt
+        <img
+          key={`photo-${index}-${photo}`}
+          src={photo}
+          className={photoClass}
+          alt={`message photo ${index + 1}`}
+          onClick={() => modalPhoto({ photoData: photoData, id: index })}
+        />
+      ))}
+    </div>
+  );
+};
+
+const ShowDocuments = ({documents})=> {
+  if (!documents) return null;
+
+  return (
+    <>
+      {documents.map((document, index) => (
+        <div
+          className={styles.documentHeaderWrap}
+          key={`doc-${index}-${document.document}`}
+        >
+          <Icon>
+            <FaArrowCircleDown
+              color="white"
+              size="35"
+              style={{ marginTop: "5px" }}
+            />
+          </Icon>
+          <div className={styles.documentTitleUpload}>
+            <a
+              key={index}
+              href={document.document}
+              download
+              className={styles.documentTitle}
+            >
+              {document}
+            </a>
+            <span className={styles.documentUpload}>Загрузить</span>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
 export default function Message({
   text,
   avatar,
@@ -16,83 +93,18 @@ export default function Message({
   reactionMessage,
   isOpenReactions,
 }) {
-  function showPhoto() {
-    if (photos === null || (Array.isArray(photos) && photos.length === 0)) {
-      return null;
-    }
-    if (photos.length === 1) {
-      return (
-        <>
-          <div className={styles.wrapPhoto}>
-            {photos.map((photo, index) => (
-              // eslint-disable-next-line jsx-a11y/img-redundant-alt
-              <img
-                key={`photo-${index}-${photo}`}
-                className={styles.photoMessage}
-                src={photo}
-                alt={`message photo ${index}`}
-                onClick={() => modalPhoto({ photoData: photoData, id: index })}
-              />
-            ))}
-          </div>
-        </>
-      );
-    } else if (photos.length === 2) {
-      return (
-        <>
-          <div className={styles.wrapPhotoTwo}>
-            {photos.map((photo, index) => (
-              // eslint-disable-next-line jsx-a11y/img-redundant-alt
-              <img
-                  key={`photo-${index}-${photo}`}
-                src={photo}
-                className={styles.photoMessageTwoPhoto}
-                alt={`message photo ${index + 1}`}
-                onClick={() => modalPhoto({ photoData: photoData, id: index })}
-              />
-            ))}
-          </div>
-        </>
-      );
-    } else if (photos.length % 2 === 0) {
-      return (
-        <>
-          <div className={styles.wrapPhotoEven}>
-            {photos.map((photo, index) => (
-              // eslint-disable-next-line jsx-a11y/img-redundant-alt
-              <img
-                key={`photo-${index}-${photo}`}
-                src={photo}
-                className={styles.photoMessageEven}
-                alt={`message photo ${index + 1}`}
-                onClick={() => modalPhoto({ photoData: photoData, id: index })}
-              />
-            ))}
-          </div>
-        </>
-      );
-    } else if (photos.length % 2 !== 0) {
-      return (
-        <>
-          <div className={styles.wrapPhotoOdd}>
-            {photos.map((photo, index) => (
-              // eslint-disable-next-line jsx-a11y/img-redundant-alt
-              <img
-                   key={`photo-${index}-${photo}`}
-                className={styles.photoMessageOdd}
-                src={photo}
-                alt={`message photo ${index + 1}`}
-                onClick={() => modalPhoto({ photoData: photoData, id: index })}
-              />
-            ))}
-          </div>
-        </>
-      );
-    } else {
-      return null;
-    }
-  }
-  const photoRender = showPhoto();
+  const onlyText =
+    text &&
+    Array.isArray(photos) &&
+    photos.length === 0 &&
+    Array.isArray(documents) &&
+    documents.length === 0;
+  const onlyDocuments =
+    !text && Array.isArray(documents) && documents.length > 0;
+  const TextDocuments =
+    text && Array.isArray(documents) && documents.length > 0;
+
+
   return (
     <div
       className={`${styles.message} ${sent ? styles.sent : styles.received}`}
@@ -100,76 +112,34 @@ export default function Message({
     >
       <img className={styles.messageAvatar} src={avatar} alt={"avatar user"} />
 
-      {text && Array.isArray(photos) && photos.length === 0 &&   Array.isArray(documents) && documents.length === 0  && (
+      {onlyText && (
         <div className={styles.messageBubbleText}>
-          <div className={styles.bubbleNameWrap}>
-            <span className={styles.bubbleNameText}>{username}</span>
-          </div>
+          <HeaderName username={username}/>
           <span>{text}</span>
-          <div className={styles.bubbleTimeWrap}>
-            <span className={styles.bubbleTimeText}>{time}</span>
-          </div>
+          <ItemTime time = {time}/>
         </div>
       )}
 
-      {!text && Array.isArray(photos) && photos.length === 0 && Array.isArray(documents) && documents.length > 0 && (
-          <div className={styles.messageBubbleDocuments}>
-            <div className={styles.bubbleNameWrap}>
-              <span className={styles.bubbleNameText}>{username}</span>
-            </div>
+      {onlyDocuments && (
+        <div className={styles.messageBubbleDocuments}>
+        <HeaderName username={username}/>
+          <ShowDocuments documents={documents}/>
+          <ItemTime time = {time}/>
+        </div>
+      )}
 
-            {documents.map((document, index) => (
-              // eslint-disable-next-line jsx-a11y/img-redundant-alt
-
-              <div className={styles.documentHeaderWrap}  key={`doc-${index}-${document.document}`}>
-                <Icon>
-                <FaArrowCircleDown color="white" size="35" style={{marginTop:"5px"}} />
-                </Icon>
-
-                <div className={styles.documentTitleUpload}>
-                <a key={index}  href={document.document} download className={styles.documentTitle}>{document}</a>
-                <span className={styles.documentUpload}>Загрузить</span>
-                </div>
-
-              </div>
-            ))}
-
-            <div className={styles.bubbleTimeWrap}>
-              <span className={styles.bubbleTimeText}>{time}</span>
-            </div>
-          </div>
-        )}
-
-        {text && Array.isArray(photos) && photos.length === 0 && Array.isArray(documents) && documents.length > 0 && (
-          <div className={styles.messageBubbleDocuments}>
-            <div className={styles.bubbleNameWrap}>
-              <span className={styles.bubbleNameText}>{username}</span>
-            </div>
-
-            {documents.map((document, index) => (
-              // eslint-disable-next-line jsx-a11y/img-redundant-alt
-
-              <div className={styles.documentHeaderWrap}  key={`doc-${index}-${document.document}`}>
-                <Icon>
-                <FaArrowCircleDown color="white" size="35" style={{marginTop:"5px"}} />
-                </Icon>
-
-                <div className={styles.documentTitleUpload}>
-                <a key={index}  href={document.document} download className={styles.documentTitle}>{document}</a>
-                <span className={styles.documentUpload}>Загрузить</span>
-                </div>
-              </div>
-            ))}
-                 <span> {text}</span>
-            <div className={styles.bubbleTimeWrap}>
-              <span className={styles.bubbleTimeText}>{time}</span>
-            </div>
-          </div>
-        )}
+      {TextDocuments && (
+        <div className={styles.messageBubbleDocuments}>
+        <HeaderName username={username}/>
+          <ShowDocuments documents={documents}/>
+          <span> {text}</span>
+          <ItemTime time = {time}/>
+        </div>
+      )}
 
       {!text && photos && (
         <div className={styles.messageBublePhoto}>
-          {photoRender}
+        <ShowPhoto photos={photos} modalPhoto={modalPhoto} photoData={photoData} />
           <div className={styles.bubbleTimeWrapPhoto}>
             <span className={styles.bubbleTimeTextPhoto}>{time}</span>
           </div>
@@ -178,30 +148,22 @@ export default function Message({
 
       {text && Array.isArray(photos) && photos.length === 1 && (
         <div className={styles.messageBubbleAll}>
-          <div className={styles.bubbleNameWrap}>
-            <span className={styles.bubbleNameText}>{username}</span>
-          </div>
-          {photoRender}
-          <div className={styles.bubbleTextOne}>
+        <HeaderName username={username}/>
+          <ShowPhoto photos={photos} modalPhoto={modalPhoto} photoData={photoData} />
+          <div className={styles.bubbleText} style={{ width: "300px" }}>
             <span>{text}</span>
-            <div className={styles.bubbleTimeWrap}>
-              <span className={styles.bubbleTimeText}>{time}</span>
-            </div>
+            <ItemTime time = {time}/>
           </div>
         </div>
       )}
 
       {text && Array.isArray(photos) && photos.length > 1 && (
         <div className={styles.messageBubbleAll}>
-          <div className={styles.bubbleNameWrap}>
-            <span className={styles.bubbleNameText}>{username}</span>
-          </div>
-          {photoRender}
+        <HeaderName username={username}/>
+          <ShowPhoto photos={photos} modalPhoto={modalPhoto} photoData={photoData} />
           <div className={styles.bubbleText}>
             <span>{text}</span>
-            <div className={styles.bubbleTimeWrap}>
-              <span className={styles.bubbleTimeText}>{time}</span>
-            </div>
+            <ItemTime time = {time}/>
           </div>
         </div>
       )}
